1f93850f67fa6ff0851d11dbf8adb6174a1dafe6
Oleksii Titov <titov292@gmail.com>
2018-06-25 14:27:03 +0300

    no message

---
+++ Alert.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace webheads\notifications;
+
+use Yii;
+use yii\helpers\Html;
+
+class Alert extends \yii\bootstrap\Widget
+{
+	public $alertTypes = [
+        'error'   => 'Error',
+        'danger'  => 'Danger',
+        'success' => 'Success',
+        'info'    => 'Info',
+        'warning' => 'Warning',
+    ];
+
+	public function init()
+    {
+    	parent::init();
+
+    	$this->registrJs();
+
+    	$logo = !empty($this->options['logo'])?intval($this->options['logo']):'';
+        $session = \Yii::$app->getSession();
+        $flashes = $session->getAllFlashes();
+        $appendCss = isset($this->options['class'])?' '.$this->options['class']:'';
+        foreach ($flashes as $type => $data) {
+			$title = (isset($this->alertTypes[$type]))?Yii::t('app', $this->alertTypes[$type]):Yii::t('app', $this->alertTypes['success']);
+			echo '<script>showNotify("'.$title.'", "'.$data.'");</script>';
+        }
+    }
+
+    function registrJs() {
+		$script = <<< JS
+
+			var Notification = window.Notification || window.mozNotification || window.webkitNotification;
+	        if (Notification) {
+	            Notification.requestPermission(function (permission) {
+	                // console.log(permission);
+	            });
+	        }
+
+		    function showNotify(notify_title, notify_text) {
+		        if (Notification) {
+		            var instance = new Notification(
+		                notify_title, {
+		                    tag: 'set',
+		                    body: notify_text,
+		                    icon: "/files/images/logo.png"
+		                }
+		            );
+
+		            instance.onclick = function () {};
+		            instance.onerror = function () {};
+		            instance.onshow = function () {};
+		            instance.onclose = function () {};
+		            
+		            setTimeout(instance.close.bind(instance), 4000);
+
+		            return false;
+		        } else {
+		            console.log('html5 notification text: ' + notify_text);
+		        }
+		    }
+		JS;
+		$this->registerJs($script, yii\web\View::POS_END);    	
+    }
+}
